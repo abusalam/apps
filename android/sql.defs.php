7578643b3ee58b19fc01e852e39c777eb8f5c17a
Latitude-E5450 <abusalamparvezalam@gmail.com>
2017-03-06 16:46:29 +0530

    Structure for APP_Logs

--- android/sql.defs.php
+++ android/sql.defs.php
@@ -4,6 +4,7 @@ function CreateSchemas() {
   $ObjDB = new MySQLiDBHelper();
   $ObjDB->ddlQuery(SQLDefs('APP_Users'));
   $ObjDB->ddlQuery(SQLDefs('APP_Register'));
+  $ObjDB->ddlQuery(SQLDefs('APP_Logs'));
   unset($ObjDB);
 }
 
@@ -34,6 +35,18 @@ function SQLDefs($ObjectName) {
         . '`RequestTime` timestamp DEFAULT CURRENT_TIMESTAMP,'
         . ' PRIMARY KEY (`RequestID`)'
         . ') ENGINE=InnoDB  DEFAULT CHARSET = utf8;';
+      break;
+
+    case 'APP_Logs':
+      $SqlDB ='CREATE TABLE `' . MySQL_Pre . $ObjectName . '` ('
+      .'`LogID` bigint(20) NOT NULL AUTO_INCREMENT,'
+      .'`IMEI` varchar(20) NOT NULL,'
+      .'`LocalIP` varchar(16) NOT NULL,'
+      .'`IP` varchar(16) NOT NULL,'
+      .'`MobileNo` varchar(15) NOT NULL,'
+      .'`AccessTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,'
+      .'PRIMARY KEY (`LogID`)'
+      .') ENGINE=InnoDB DEFAULT CHARSET=utf8';
       break;
   }
 
