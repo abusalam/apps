24401c1e473dc441b8f58b1722f3db7f0169c2ef
Latitude-E5450 <abusalamparvezalam@gmail.com>
2017-03-06 19:07:51 +0530

    JSON Structure Validation for MprAPI

--- mpr/android/api.php
+++ mpr/android/api.php
@@ -8,8 +8,9 @@ require_once(__DIR__ . '/MprAPI.php');
 $RT = time();
 WebLib::CreateDB();
 $json     = file_get_contents('php://input');
-$jsonData = json_decode($json);
-
-$mAPI = new MprAPI($jsonData);
-$mAPI();
+$jsonData = @json_decode($json);
+if ($jsonData !== NULL) {
+  $mAPI = new MprAPI($jsonData);
+  $mAPI();
+}
 exit();
