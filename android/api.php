ae6b5d9f7f1338459837342fe32399e78b150db8
Latitude-E5450 <abusalamparvezalam@gmail.com>
2017-03-07 12:11:20 +0530

    Set default time zone

--- android/api.php
+++ android/api.php
@@ -1,16 +1,16 @@
 <?php
 
-  ini_set('display_errors', '1');
-  error_reporting(E_ALL);
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+date_default_timezone_set('Asia/Kolkata');
+require_once(__DIR__ . '/AndroidAPI.php');
 
-  require_once(__DIR__ . '/AndroidAPI.php');
-
-  $RT = time();
-  WebLib::CreateDB();
-  $json     = file_get_contents('php://input');
-  $jsonData = @json_decode($json);
-  if ($jsonData !== NULL) {
-    $mAPI = new AndroidAPI($jsonData);
-    $mAPI();
-  }
-  exit();
+$RT = time();
+WebLib::CreateDB();
+$json     = file_get_contents('php://input');
+$jsonData = @json_decode($json);
+if ($jsonData !== null) {
+  $mAPI = new AndroidAPI($jsonData);
+  $mAPI();
+}
+exit();
--- mpr/android/api.php
+++ mpr/android/api.php
@@ -2,14 +2,14 @@
 
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
-
+date_default_timezone_set('Asia/Kolkata');
 require_once(__DIR__ . '/MprAPI.php');
 
 $RT = time();
 WebLib::CreateDB();
 $json     = file_get_contents('php://input');
 $jsonData = @json_decode($json);
-if ($jsonData !== NULL) {
+if ($jsonData !== null) {
   $mAPI = new MprAPI($jsonData);
   $mAPI();
 }
