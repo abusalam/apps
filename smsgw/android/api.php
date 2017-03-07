e46a71c84ff5ae7437d1b8c4f1451eb80b6f612b
Latitude-E5450 <abusalamparvezalam@gmail.com>
2017-03-07 12:26:44 +0530

    JSON Structure Validation for MprAPI

--- smsgw/android/api.php
+++ smsgw/android/api.php
@@ -2,14 +2,15 @@
 
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
-
+date_default_timezone_set('Asia/Kolkata');
 require_once(__DIR__ . '/MessageAPI.php');
 
 $RT = time();
 WebLib::CreateDB();
 $json     = file_get_contents('php://input');
-$jsonData = json_decode($json);
-
-$mAPI = new MessageAPI($jsonData);
-$mAPI();
+$jsonData = @json_decode($json);
+if ($jsonData !== null) {
+  $mAPI = new MessageAPI($jsonData);
+  $mAPI();
+}
 exit();
