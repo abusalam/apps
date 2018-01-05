0d9f5d3c6b70f8bb73f54e5fdee53b3059d1a70d
Latitude-E5450 <abusalamparvezalam@gmail.com>
2018-01-05 15:43:21 +0530

    POC (III): Insecure Direct Object Reference

--- mpr/SchemesData.php
+++ mpr/SchemesData.php
@@ -3,7 +3,7 @@ if (WebLib::GetVal($_POST, 'FormToken') !== null) {
   if (WebLib::GetVal($_POST, 'FormToken') == WebLib::GetVal($_SESSION, 'FormToken')) {
     if (isset($_POST['BtnCreScheme']) == 1) {
       $DB                      = new MySQLiDBHelper();
-      $tableData['SchemeName'] = $_POST['txtSchemeName'];
+      $tableData['SchemeName'] = WebLib::GetVal($_POST, 'txtSchemeName');
       $tableData['UserMapID']  = $_SESSION['UserMapID'];
       $SchemeID                = $DB->insert(MySQL_Pre . 'MPR_Schemes', $tableData);
       if ($SchemeID > 0) {
@@ -14,17 +14,24 @@ if (WebLib::GetVal($_POST, 'FormToken') !== null) {
     }
 
     if (isset($_POST['BtnScheme']) == 1) {
-      $DB                    = new MySQLiDBHelper();
-      $tableData['SchemeID'] = $_POST['Scheme'];
-      $tableData['Amount']   = $_POST['txtAmount'];
-      $tableData['OrderNo']  = $_POST['txtOrderNo'];
-      $tableData['Date']     = WebLib::ToDBDate($_POST['txtDate']);
-      $tableData['Year']     = $_POST['txtYear'];
-      $SchemeID              = $DB->insert(MySQL_Pre . 'MPR_SchemeAllotments', $tableData);
-      if ($SchemeID > 0) {
-        $_SESSION['Msg'] = "Allotment Saved Successfully!";
+      $DB = new MySQLiDBHelper();
+      $DB->where('UserMapID', $_SESSION['UserMapID']);
+      $DB->where('SchemeID', WebLib::GetVal($_POST, 'Scheme'));
+      $Schemes = $DB->get(MySQL_Pre . 'MPR_Schemes');
+      if (count($Schemes) > 0) {
+        $tableData['SchemeID'] = $_POST['Scheme'];
+        $tableData['Amount']   = $_POST['txtAmount'];
+        $tableData['OrderNo']  = $_POST['txtOrderNo'];
+        $tableData['Date']     = WebLib::ToDBDate($_POST['txtDate']);
+        $tableData['Year']     = $_POST['txtYear'];
+        $SchemeID              = $DB->insert(MySQL_Pre . 'MPR_SchemeAllotments', $tableData);
+        if ($SchemeID > 0) {
+          $_SESSION['Msg'] = "Allotment Saved Successfully!";
+        } else {
+          $_SESSION['Msg'] .= "Unable to Save Allotment!";
+        }
       } else {
-        $_SESSION['Msg'] .= "Unable to Save Allotment!";
+        $_SESSION['Msg'] .= "Invalid Scheme: Unable to Save Allotment!";
       }
     }
   } else {
--- mpr/WorkData.php
+++ mpr/WorkData.php
@@ -1,20 +1,27 @@
 <?php
-if (WebLib::GetVal($_POST, 'FormToken') != NULL) {
+if (WebLib::GetVal($_POST, 'FormToken') != null) {
   if (WebLib::GetVal($_POST, 'FormToken') == WebLib::GetVal($_SESSION, 'FormToken')) {
     switch (WebLib::GetVal($_POST, 'CmdAction')) {
 
       case 'Create Work':
-        $DB                           = new MySQLiDBHelper();
-        $tableData['SchemeID']        = $_POST['Scheme'];
-        $tableData['MprMapID']        = $_POST['MprMapID'];
-        $tableData['WorkDescription'] = $_POST['txtWork'];
-        $tableData['EstimatedCost']   = $_POST['txtCost'];
-        $tableData['WorkRemarks']     = $_POST['txtWorkRemarks'];
-        $SchemeID                     = $DB->insert(MySQL_Pre . 'MPR_Works', $tableData);
-        if($SchemeID){
-          $_SESSION['Msg'] = "Work Created!";
+        $DB = new MySQLiDBHelper();
+        $DB->where('UserMapID', $_SESSION['UserMapID']);
+        $DB->where('SchemeID', WebLib::GetVal($_POST, 'Scheme'));
+        $Schemes = $DB->get(MySQL_Pre . 'MPR_Schemes');
+        if (count($Schemes) > 0) {
+          $tableData['SchemeID']        = $_POST['Scheme'];
+          $tableData['MprMapID']        = $_POST['MprMapID'];
+          $tableData['WorkDescription'] = $_POST['txtWork'];
+          $tableData['EstimatedCost']   = $_POST['txtCost'];
+          $tableData['WorkRemarks']     = $_POST['txtWorkRemarks'];
+          $SchemeID                     = $DB->insert(MySQL_Pre . 'MPR_Works', $tableData);
+          if ($SchemeID) {
+            $_SESSION['Msg'] = "Work Created!";
+          } else {
+            $_SESSION['Msg'] = "Unable to Create Work!";
+          }
         } else {
-          $_SESSION['Msg'] = "Unable to Create Work!";
+          $_SESSION['Msg'] .= "Invalid Scheme: Unable to Create Work!";
         }
         unset($tableData);
         unset($DB);
@@ -22,16 +29,23 @@ if (WebLib::GetVal($_POST, 'FormToken') != NULL) {
 
       case 'Release Fund':
         $DB                           = new MySQLiDBHelper();
-        $tableData['WorkID']          = $_POST['WorkID'];
-        $tableData['SanctionOrderNo'] = $_POST['txtOrderNo'];
-        $tableData['SanctionDate']    = WebLib::ToDBDate($_POST['txtDate']);
-        $tableData['SanctionAmount']  = $_POST['txtAmount'];
-        $tableData['SanctionRemarks'] = $_POST['txtSanctionRemarks'];
-        $SchemeID                     = $DB->insert(MySQL_Pre . 'MPR_Sanctions', $tableData);
-        if($SchemeID){
-          $_SESSION['Msg'] = "Fund Released!";
+        $DB->where('UserMapID', $_SESSION['UserMapID']);
+        $DB->where('SchemeID', WebLib::GetVal($_POST, 'WorkID'));
+        $Works = $DB->get(MySQL_Pre . 'MPR_ViewUserWorks');
+        if (count($Works) > 0) {
+          $tableData['WorkID']          = $_POST['WorkID'];
+          $tableData['SanctionOrderNo'] = $_POST['txtOrderNo'];
+          $tableData['SanctionDate']    = WebLib::ToDBDate($_POST['txtDate']);
+          $tableData['SanctionAmount']  = $_POST['txtAmount'];
+          $tableData['SanctionRemarks'] = $_POST['txtSanctionRemarks'];
+          $SchemeID                     = $DB->insert(MySQL_Pre . 'MPR_Sanctions', $tableData);
+          if ($SchemeID) {
+            $_SESSION['Msg'] = "Fund Released!";
+          } else {
+            $_SESSION['Msg'] = "Unable to Release Fund!";
+          }
         } else {
-          $_SESSION['Msg'] = "Unable to Release Fund!";
+          $_SESSION['Msg'] .= "Invalid Work: Unable to Release Fund!";
         }
         unset($tableData);
         unset($DB);
